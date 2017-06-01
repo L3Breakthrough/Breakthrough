61b33b7fb26f321b3e24dcd7e7a19f98538f3fed
DucoroyL <l.ducoroy@gmail.com>
2017-06-01 11:10:55 +0200

    Mise en place des logs closed #2

--- src/main_console.cpp
+++ src/main_console.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <glog/logging.h> //Pour logs
 using namespace std;
 
 int main(){
+	/// initialisation des logs
+	google::SetLogDestination(google::GLOG_INFO, "log_breakthrough");
+	google::InitGoogleLogging(argv[0]);
 	cout<<"Hello World"<<endl;
 return 0;
 }
